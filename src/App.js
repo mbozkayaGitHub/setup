edc97233a0e747547683abe26e02d620ab7e47e0
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-03-24 17:58:23 +0300

    AppBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-import NavBar from "./components/AppBar"
+import AppBar from "./components/AppBar"
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
@@ -9,7 +9,11 @@ const url = 'https://course-api.com/react-tours-project'
 
 
 function App() {
-  return <h2>Tours Project Setup</h2>
+  return  <div>
+    <h2>Tours Project Setup</h2>
+    <AppBar/>
+  </div>
+  
      
 }
 
--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,12 @@
+import { Box } from '@mui/system'
 import React from 'react'
 
 const AppBar = () => {
   return (
-    <div>Places to Visit</div>
+
+
+   <Box bgcolor="red" mt={4} flex={1} p={2}>Place to Visit</Box>
+    
   )
 }
 
